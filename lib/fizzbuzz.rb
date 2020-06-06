e959088d9f5d67b4985f0de5faf63ad4b254536b
Joanne Chen <joanne_r_chen@yahoo.com>
2020-06-06 19:10:30 +0100

    fizzbuzz method

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,14 @@
+# lib/fizzbuzz.#!/usr/bin/env ruby -wKU
+def fizzbuzz(number)
+  if number%3 == 0 && number%5 == 0
+    "fizzbuzz"
+  elsif number%3 == 0
+    "fizz"
+
+  elsif number%5 == 0
+   "buzz"
+
+  else
+   number
+  end
+end
